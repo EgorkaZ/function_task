4e88a2f96600b0cd6e3adebba4daaf093c2e5dfb
EgorkaZ <zudin.0197@gmail.com>
2020-10-12 00:34:49 +0300

    function implementation

--- function.h
+++ function.h
@@ -1,31 +1,85 @@
 #pragma once
 
+#include "function_traits.h"
+
+#include <exception>
+#include <memory>
+
+struct bad_function_call : public std::exception
+{
+    const char * what() const noexcept override
+    {
+        return "bad function call";
+    }
+};
+
 template <typename F>
 struct function;
 
 template <typename R, typename... Args>
 struct function<R (Args...)>
 {
-    function() noexcept;
+    function() = default;
 
-    function(function const& other);
-    function(function&& other) noexcept;
+    function(function const& other)
+        : m_caller(other ? other.m_caller->make_copy() : nullptr)
+    {
+    }
 
-    template <typename T>
-    function(T val);
-
-    function& operator=(function const& rhs);
-    function& operator=(function&& rhs) noexcept;
-
-    ~function();
-
-    explicit operator bool() const noexcept;
-
-    R operator()(Args... args) const;
+    function(function&& other) noexcept
+        : m_caller(std::move(other.m_caller))
+    {
+    }
 
     template <typename T>
-    T* target() noexcept;
+    function(T val)
+        : m_caller(std::make_unique<detail::function_caller<T, R(Args...)>>(std::move(val)))
+    {
+    }
+
+    function& operator=(function const& rhs)
+    {
+        if (this != &rhs) {
+            if (rhs) {
+                m_caller.reset(rhs.m_caller->make_copy());
+            } else {
+                m_caller.reset();
+            }
+        }
+        return *this;
+    }
+    function& operator=(function&& rhs) noexcept
+    {
+        m_caller.swap(rhs.m_caller);
+        return *this;
+    }
+
+    explicit operator bool() const noexcept
+    {
+        return m_caller.get();
+    }
+
+    R operator()(Args... args) const
+    {
+        if (m_caller.get()) {
+            return m_caller->invoke(std::forward<Args>(args)...);
+        } else {
+            throw bad_function_call();
+        }
+    }
 
     template <typename T>
-    T const* target() const noexcept;
+    T* target() noexcept
+    {
+        return (*this) ? m_caller->template get_func_ptr<T>() : nullptr;
+    }
+
+    template <typename T>
+    T const* target() const noexcept
+    {
+        return (*this) ? m_caller->template get_func_ptr<T>() : nullptr;
+    }
+
+private:
+    std::unique_ptr<detail::function_caller_base<R(Args...)>> m_caller;
 };
---
+++ function_traits.h
@@ -0,0 +1,158 @@
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+namespace detail {
+
+constexpr std::size_t SizeOfStorage = sizeof(void *);
+constexpr std::size_t AlignOfStorage = alignof(void *);
+
+template <class T>
+constexpr bool fits_storage = sizeof(T) <= SizeOfStorage &&
+                              alignof(T) % AlignOfStorage &&
+                              std::is_nothrow_move_constructible<T>::value;
+
+using storage_t = std::aligned_storage<SizeOfStorage, AlignOfStorage>;
+
+template <class Functor, bool fits_small = fits_storage<Functor>>
+struct func_storage
+{
+
+    func_storage(const Functor & func) noexcept(fits_small && std::is_nothrow_copy_constructible_v<Functor>)
+    {
+        forward_assign(func);
+    }
+
+    func_storage(Functor && func) noexcept(fits_small && std::is_nothrow_move_constructible_v<Functor>)
+    {
+        forward_assign(std::move(func));
+    }
+
+    Functor * get_func_ptr() noexcept
+    {
+        return functor_from_storage<false>(m_storage);
+    }
+
+    const Functor & get_func() const noexcept
+    {
+        return *(functor_from_storage<true>(m_storage));
+    }
+
+    void destruct() noexcept(std::is_nothrow_destructible_v<Functor>)
+    {
+        get_func_ptr()->~Functor();
+    }
+
+    ~func_storage() noexcept(std::is_nothrow_destructible_v<Functor>)
+    {
+        get_func_ptr()->~Functor();
+    }
+
+private:
+
+    template <bool is_const, class C>
+    using cond_const_ptr = std::conditional_t<is_const, const C *, C *>;
+
+    template <bool is_const, class C>
+    using cond_const_ref = std::conditional_t<is_const, const C &, C &>;
+
+    template <bool is_const>
+    static cond_const_ptr<is_const, Functor> functor_from_storage(cond_const_ref<is_const, storage_t> storage) noexcept
+    {
+        using ReturnedFunctor = cond_const_ptr<is_const, Functor>;
+        if constexpr (fits_small) {
+            return reinterpret_cast<cond_const_ptr<is_const, Functor>>(&storage);
+        } else {
+            return reinterpret_cast<ReturnedFunctor>(reinterpret_cast<cond_const_ref<is_const, std::ptrdiff_t>>(storage));
+        }
+    }
+
+    template <class F>
+    void forward_assign(F && f) noexcept(fits_small && std::is_nothrow_constructible_v<Functor, F &&>)
+    {
+        if constexpr (fits_small) {
+            new (&m_storage) Functor(std::forward<F>(f));
+        } else {
+            reinterpret_cast<Functor * &>(m_storage) = new Functor(std::forward<F>(f));
+        }
+    }
+
+    storage_t m_storage;
+};
+
+template <class Signature>
+struct function_caller_base;
+
+template <class Functor, class Signature>
+struct function_caller;
+
+
+template <class Ret, class... Args>
+struct function_caller_base<Ret(Args...)>
+{
+    template <class Functor>
+    using Derived = function_caller<Functor, Ret(Args...)>;
+
+    virtual Ret invoke(Args && ...) = 0;
+
+    virtual function_caller_base * make_copy() const = 0;
+
+    template <class Functor>
+    Functor * get_func_ptr()
+    {
+        auto as_derived = dynamic_cast<Derived<Functor> *>(this);
+        return as_derived ? as_derived->get_func_ptr() : nullptr;
+    }
+
+    template <class Functor>
+    const Functor & get_func() const
+    {
+        return reinterpret_cast<const Derived<Functor> *>(this)->get_func();
+    }
+
+    virtual ~function_caller_base() = default;
+};
+
+template <class Functor, class Ret, class... Args>
+struct function_caller<Functor, Ret(Args...)> final : function_caller_base<Ret(Args...)>
+{
+    using Base = function_caller_base<Ret(Args...)>;
+
+    function_caller() = default;
+
+    function_caller(const Functor & func)
+        : m_storage(func)
+    {
+    }
+
+    function_caller(Functor && func)
+        : m_storage(std::move(func))
+    {
+    }
+
+    Ret invoke(Args && ... args) final
+    {
+        return m_storage.get_func()(std::forward<Args>(args)...);
+    }
+
+    Base * make_copy() const final
+    {
+        return new function_caller(get_func());
+    }
+
+    Functor * get_func_ptr() noexcept
+    {
+        return m_storage.get_func_ptr();
+    }
+
+    const Functor & get_func() const noexcept
+    {
+        return m_storage.get_func();
+    }
+
+private:
+    func_storage<Functor> m_storage;
+};
+
+}; // detail
+
